3ed70a4589d772dce6c3cb342efc99f7f7229885
Ivan Villena Sanchez <55782944+IvanVillena@users.noreply.github.com>
2019-09-25 21:33:55 +0200

    Delete ejercicio3.java

--- ejercicio3.java
+++
@@ -1,8 +0,0 @@
-public class ejercicio3 {
-    public static void main(String[] args) {
-        String nombre = "Iván Villena Sánchez";
-        String direccion = "c/ Enrique de Egas, 23";
-        String telefono = "648781170";
-        System.out.println("Me llamo " + nombre + " vivo en " + direccion + " y mi número de teléfono es " + telefono);
-    }
-}

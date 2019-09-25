8e1c9b625eb27b87d3bb2365ab9b9d41454d7413
Ivan Villena Sanchez <55782944+IvanVillena@users.noreply.github.com>
2019-09-25 21:33:43 +0200

    Delete ejercicio2.java

--- ejercicio2.java
+++
@@ -1,6 +0,0 @@
-public class ejercicio2 {
-    public static void main(String[] asgr) {
-        String nombre = "Iván Villena Sánchez";
-        System.out.println("Mi nombre es " + nombre);
-    }
-}

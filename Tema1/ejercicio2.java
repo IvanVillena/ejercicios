e204ccdb188d29e858effd016bd149d394e2017a
Ivan Villena Sanchez <55782944+IvanVillena@users.noreply.github.com>
2019-09-25 21:31:06 +0200

    Add files via upload

---
+++ Tema1/ejercicio2.java
@@ -0,0 +1,10 @@
+public class ejercicio2 {
+
+  public static void main(String[] args) {
+   
+   
+    System.out.println("Me llamo Iván Villena Sánchez");
+    System.out.println("Mi dirección es c/Enrique de Egas, 23");
+    System.out.println("Mi número de teléfono es 648781170");
+  }
+}

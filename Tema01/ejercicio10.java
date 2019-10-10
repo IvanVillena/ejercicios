0873b1051e1ef1a7ecd67821d4c5e65715ee4a43
Ivan Villena Sanchez <55782944+IvanVillena@users.noreply.github.com>
2019-10-10 09:58:27 +0200

    Add files via upload

---
+++ Tema01/ejercicio10.java
@@ -0,0 +1,12 @@
+public class ejercicio10 {
+	public static void main(String[] args) {
+
+		System.out.println("   \033[33m██ ██   ");
+		System.out.println("  ███████ ");
+		System.out.println(" █████████ ");
+		System.out.println("████\033[36m▒\033[33m██\033[36m▒\033[33m███ ");
+		System.out.println("███████████ ");
+		System.out.println(" █████\033[37m░░\033[33m██");
+		System.out.println("   █████ ");
+	}
+}

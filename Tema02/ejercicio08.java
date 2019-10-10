ec966f026831a8493185bd22bf59cb2bf4de67f3
Ivan Villena Sanchez <55782944+IvanVillena@users.noreply.github.com>
2019-10-10 10:13:25 +0200

    Add files via upload

---
+++ Tema02/ejercicio08.java
@@ -0,0 +1,15 @@
+public class ejercicio08 {
+	public static void main(String[] args) {
+		
+		char q = 'q';
+		char u = 'u';
+		char e = 'e';
+		char s = 's';
+		char o = 'o';
+		
+		String queso = (q + "" + u + "" + e + "" + s + "" + o);
+		
+		System.out.println(queso);
+	
+	}
+}
